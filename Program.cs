3ce378fa8a3afd9148b7566d9c78b12dfa71b7d2
fhr3n <56928892+fhr3n@users.noreply.github.com>
2020-07-01 23:09:28 +0700

    Collection #2
    with CRUD

--- Program.cs
+++ Program.cs
@@ -10,6 +10,8 @@ namespace Collection
     {
         static void Main(string[] args)
         {
+            /*
+            "Without CRUD"
             Karyawan_Harian karyawan1 = new Karyawan_Harian();
             Karyawan_Tetap karyawan2 = new Karyawan_Tetap();
             Sales karyawan3 = new Sales();
@@ -36,7 +38,167 @@ namespace Collection
                 counter++;
             }
 
-            Console.ReadKey();
+            Console.ReadKey(); */
+            
+            List<Karyawan_Tetap> karyawantetap = new List<Karyawan_Tetap>();
+            List<Karyawan_Harian> karyawanharian = new List<Karyawan_Harian>();
+            List<Sales> sales = new List<Sales>();
+
+            menu:
+            Console.WriteLine("1. Input Data Karyawan");
+            Console.WriteLine("2. Lihat Data Karyawan");
+            Console.WriteLine("3. Hapus Data Karyawan");
+            Console.WriteLine("4. Keluar");
+            Console.WriteLine();
+            Console.Write("Silahkan Pilih [1-4] : ");
+            int pilihan;
+            int pil;
+            pilihan = int.Parse(Console.ReadLine());
+
+            if (pilihan == 1)
+            {
+                Console.WriteLine("\n1. Input Data Karyawan Tetap");
+                Console.WriteLine("2. Input Data Karyawan Harian");
+                Console.WriteLine("3. Input Data Sales");
+                Console.WriteLine();
+                Console.Write("Silahkan Pilih [1-3] : ");
+                pil = int.Parse(Console.ReadLine());
+
+                if (pil == 1)
+                {
+                    string nik, nama;
+                    double gajibulanan;
+                    Console.WriteLine("Masukan Nik : ");
+                    nik = Console.ReadLine();
+                    Console.WriteLine("Masukan Nama : ");
+                    nama = Console.ReadLine();
+                    Console.WriteLine("Masukan Gaji Bulanan : ");
+                    gajibulanan = int.Parse(Console.ReadLine());
+
+                    karyawantetap.Add(new Karyawan_Tetap() { nik = nik, nama = nama, gajiBulanan = gajibulanan });
+                }
+
+                else if (pil == 2)
+                {
+                    string nik, nama;
+                    double jam;
+                    double upah;
+                    Console.WriteLine("Masukan Nik : ");
+                    nik = Console.ReadLine();
+                    Console.WriteLine("Masukan Nama : ");
+                    nama = Console.ReadLine();
+                    Console.WriteLine("Masukan Jumlah Jam : ");
+                    jam = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Masukan Jumlah Upah : ");
+                    upah = int.Parse(Console.ReadLine());
+
+                    karyawanharian.Add(new Karyawan_Harian() { nik = nik, nama = nama, jumlahJamKerja = jam, upahPerJam = upah });
+                }
+
+                else if (pil == 3)
+                {
+                    string nik, nama;
+                    double jual;
+                    double komisi;
+                    Console.WriteLine("Masukan Nik : ");
+                    nik = Console.ReadLine();
+                    Console.WriteLine("Masukan Nama : ");
+                    nama = Console.ReadLine();
+                    Console.WriteLine("Masukan Jumlah Penjualan : ");
+                    jual = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Masukan Komisi : ");
+                    komisi = int.Parse(Console.ReadLine());
+
+                    sales.Add(new Sales() { nik = nik, nama = nama, jumlahPenjualan = jual, komisi = komisi });
+                }
+                Console.WriteLine("Tekan Enter Untuk Lanjut");
+                Console.ReadKey(true);
+                Console.Clear();
+                goto menu;
+            }
+
+            else if (pilihan == 2)
+            {
+                Console.WriteLine("Data Karyawan");
+                Console.WriteLine();
+                int no = 1;
+                foreach (Karyawan karyawan in karyawantetap)
+                {
+                    Console.WriteLine("{0}. Nik : {1}, Nama : {2}, Gaji : {3}, Karyawan Tetap",
+                    no, karyawan.nik, karyawan.nama, karyawan.gaji());
+                    no++;
+                }
+
+                foreach (Karyawan karyawan in karyawanharian)
+                {
+                    Console.WriteLine("{0}. Nik : {1}, Nama : {2}, Gaji : {3}, Karyawan Harian",
+                    no, karyawan.nik, karyawan.nama, karyawan.gaji());
+                    no++;
+                }
+
+                foreach (Karyawan karyawan in sales)
+                {
+                    Console.WriteLine("{0}. Nik : {1}, Nama : {2}, Gaji : {3}, Sales",
+                    no, karyawan.nik, karyawan.nama, karyawan.gaji());
+                    no++;
+                }
+
+                Console.WriteLine("Tekan Enter Untuk Lanjut");
+                Console.ReadKey(true);
+                Console.Clear();
+                goto menu;
+            }
+            else if (pilihan == 3)
+            {
+                Console.WriteLine("Hapus Karyawan");
+                Console.WriteLine();
+                string hapus;
+                Console.WriteLine("Masukan Nik yang ingin dihapus : ");
+                hapus = Console.ReadLine();
+                int i = 0;
+                foreach (Karyawan karyawan in karyawantetap)
+                {
+                    if (hapus == karyawan.nik)
+                    {
+                        karyawantetap.RemoveAt(i);
+                        break;
+                    }
+                    i++;
+                }
+                i = 0;
+                foreach (Karyawan karyawan in karyawanharian)
+                {
+                    if (hapus == karyawan.nik)
+                    {
+                        karyawanharian.RemoveAt(i);
+                        break;
+                    }
+                    i++;
+                }
+                i = 0;
+                foreach (Karyawan karyawan in sales)
+                {
+                    if (hapus == karyawan.nik)
+                    {
+                        sales.RemoveAt(i);
+                        break;
+                    }
+                    i++;
+                }
+                Console.WriteLine("Data Berhasil dihapus");
+                Console.WriteLine("Tekan Enter Untuk Lanjut");
+                Console.ReadKey(true);
+                Console.Clear();
+                goto menu;
+            }
+
+            else
+            {
+                Console.WriteLine("Terima Kasih");
+            }
+
+            Console.ReadKey(true);
+            Console.Clear();
 
         }
     }
